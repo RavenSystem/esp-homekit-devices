1a83c1c68546800955894f0989c7839aa5420187
José Antonio Jiménez Campos <ravensystem@icloud.com>
2018-01-28 13:21:31 +0100

    Uses define instead const

--- devices/Sonoff_Basic/main.c
+++ devices/Sonoff_Basic/main.c
@@ -35,14 +35,12 @@
 #include <homekit/characteristics.h>
 #include <wifi_config.h>
 
-// The GPIO pin that is connected to the relay on the Sonoff Basic.
-const int relay_gpio = 12;
-// The GPIO pin that is connected to the LED on the Sonoff Basic.
-const int led_gpio = 13;
-// The GPIO pin that is connected to an optional external switch with ground.
-const int switch_gpio = 14;
-// The GPIO pin that is connected to the button on the Sonoff Basic.
-const int button_gpio = 0;
+// GPIO
+#define BUTTON_GPIO     0
+#define LED_GPIO        13
+#define RELAY_GPIO      12
+#define SWITCH_GPIO     14
+
 // The minimum amount that has to occur between each button press.
 const uint16_t button_event_debounce_time = 350 / portTICK_PERIOD_MS;
 // The last time the button was pressed, in ticks.
@@ -51,35 +49,32 @@ uint32_t last_button_event_time;
 uint8_t times = 0;
 
 void relay_write(bool on) {
-    gpio_write(relay_gpio, on ? 1 : 0);
+    gpio_write(RELAY_GPIO, on ? 1 : 0);
 }
 
 void led_write(bool on) {
-    gpio_write(led_gpio, on ? 0 : 1);
+    gpio_write(LED_GPIO, on ? 0 : 1);
 }
 
 void switch_on_callback(homekit_characteristic_t *_ch, homekit_value_t on, void *context);
 
 void button_intr_callback(uint8_t gpio);
 
-homekit_characteristic_t switch_on = HOMEKIT_CHARACTERISTIC_(
-                                                             ON,
-                                                             false,
-                                                             .callback=HOMEKIT_CHARACTERISTIC_CALLBACK(switch_on_callback)
-                                                             );
+homekit_characteristic_t switch_on = HOMEKIT_CHARACTERISTIC_(ON, false, .callback=HOMEKIT_CHARACTERISTIC_CALLBACK(switch_on_callback));
 
 void gpio_init() {
-    gpio_enable(led_gpio, GPIO_OUTPUT);
+    gpio_enable(LED_GPIO, GPIO_OUTPUT);
     led_write(false);
-    gpio_enable(relay_gpio, GPIO_OUTPUT);
+    
+    gpio_enable(RELAY_GPIO, GPIO_OUTPUT);
     relay_write(switch_on.value.bool_value);
     
-    gpio_set_pullup(button_gpio, true, true);
-    gpio_set_interrupt(button_gpio, GPIO_INTTYPE_EDGE_NEG, button_intr_callback);
+    gpio_set_pullup(BUTTON_GPIO, true, true);
+    gpio_set_interrupt(BUTTON_GPIO, GPIO_INTTYPE_EDGE_NEG, button_intr_callback);
     
-    gpio_enable(switch_gpio, GPIO_INPUT);
-    gpio_set_pullup(switch_gpio, true, true);
-    gpio_set_interrupt(switch_gpio, GPIO_INTTYPE_EDGE_ANY, button_intr_callback);
+    gpio_enable(SWITCH_GPIO, GPIO_INPUT);
+    gpio_set_pullup(SWITCH_GPIO, true, true);
+    gpio_set_interrupt(SWITCH_GPIO, GPIO_INTTYPE_EDGE_ANY, button_intr_callback);
 }
 
 void switch_on_callback(homekit_characteristic_t *_ch, homekit_value_t on, void *context) {
@@ -87,10 +82,10 @@ void switch_on_callback(homekit_characteristic_t *_ch, homekit_value_t on, void 
     relay_write(switch_on.value.bool_value);
 }
 
-void switch_identify_task(void *_args) {
-    // Identify Sonoff by turning on builtin LED for 2 seconds
+void identify_task(void *_args) {
+    // Identify Sonoff by turning on builtin LED for 3 seconds
     led_write(true);
-    vTaskDelay(2000 / portTICK_PERIOD_MS);
+    vTaskDelay(3000 / portTICK_PERIOD_MS);
     led_write(false);
     vTaskDelete(NULL);
 }
@@ -99,10 +94,13 @@ void switch_reset_task(void *_args) {
     // Remove HomeKit config and reset device
     printf(">>> Resetting HomeKit setup\n");
     homekit_server_reset();
+    
+    // Turn on LED for 2 seconds
     led_write(true);
-    vTaskDelay(3000 / portTICK_PERIOD_MS);
+    vTaskDelay(2000 / portTICK_PERIOD_MS);
     led_write(false);
-    printf(">>> Resetting device\n");
+    
+    printf(">>> Restarting device\n");
     sdk_system_restart();
     vTaskDelete(NULL);
 }
@@ -112,7 +110,7 @@ void button_intr_callback(uint8_t gpio) {
     
     if ((now - last_button_event_time) < button_event_debounce_time) {
         // Debounce time, ignore switching events, but cheking to perform a HomeKit config reset
-        if (gpio == button_gpio) {
+        if (gpio == BUTTON_GPIO) {
             times++;
             if (times == 5) {
                 xTaskCreate(switch_reset_task, "Switch reset", 128, NULL, 1, NULL);
@@ -127,13 +125,13 @@ void button_intr_callback(uint8_t gpio) {
     }
 }
 
-void switch_identify(homekit_value_t _value) {
+void identify(homekit_value_t _value) {
     printf(">>> Identifying\n");
-    xTaskCreate(switch_identify_task, "Switch identify", 128, NULL, 2, NULL);
+    xTaskCreate(identify_task, "Switch identify", 128, NULL, 2, NULL);
 }
 
 homekit_characteristic_t name = HOMEKIT_CHARACTERISTIC_(NAME, "Sonoff Switch");
-homekit_characteristic_t serial = HOMEKIT_CHARACTERISTIC_(SERIAL_NUMBER, "Sonoff N/A");
+homekit_characteristic_t serial = HOMEKIT_CHARACTERISTIC_(SERIAL_NUMBER, "SonoffB N/A");
 
 homekit_accessory_t *accessories[] = {
     HOMEKIT_ACCESSORY(.id=1, .category=homekit_accessory_category_switch, .services=(homekit_service_t*[]){
@@ -142,8 +140,8 @@ homekit_accessory_t *accessories[] = {
             HOMEKIT_CHARACTERISTIC(MANUFACTURER, "iTEAD"),
             &serial,
             HOMEKIT_CHARACTERISTIC(MODEL, "Sonoff Basic"),
-            HOMEKIT_CHARACTERISTIC(FIRMWARE_REVISION, "1.0.0"),
-            HOMEKIT_CHARACTERISTIC(IDENTIFY, switch_identify),
+            HOMEKIT_CHARACTERISTIC(FIRMWARE_REVISION, "1.0"),
+            HOMEKIT_CHARACTERISTIC(IDENTIFY, identify),
             NULL
         }),
         HOMEKIT_SERVICE(SWITCH, .primary=true, .characteristics=(homekit_characteristic_t*[]){
@@ -167,16 +165,16 @@ void create_accessory_name() {
     sdk_wifi_get_macaddr(STATION_IF, macaddr);
     
     // Use MAC address as part of accesory name and serial number
-    uint8_t name_len = snprintf(NULL, 0, "Sonoff %02X%02X%02X", macaddr[3], macaddr[4], macaddr[5]);
+    uint8_t name_len = snprintf(NULL, 0, "SonoffB %02X%02X%02X", macaddr[3], macaddr[4], macaddr[5]);
     char *name_value = malloc(name_len+1);
-    snprintf(name_value, name_len+1, "Sonoff %02X%02X%02X", macaddr[3], macaddr[4], macaddr[5]);
+    snprintf(name_value, name_len+1, "SonoffB %02X%02X%02X", macaddr[3], macaddr[4], macaddr[5]);
     
     name.value = HOMEKIT_STRING(name_value);
-    serial.value = HOMEKIT_STRING(name_value);
+    serial.value = name.value;
 }
 
 void on_wifi_ready() {
-    xTaskCreate(switch_identify_task, "Switch identify", 128, NULL, 1, NULL);
+    xTaskCreate(identify_task, "Identify", 128, NULL, 1, NULL);
     
     create_accessory_name();
         
@@ -187,7 +185,7 @@ void on_wifi_ready() {
 void user_init(void) {
     uart_set_baud(0, 115200);
     
-    wifi_config_init("Sonoff", NULL, on_wifi_ready);
+    wifi_config_init("SonoffB", NULL, on_wifi_ready);
     
     gpio_init();
 }
