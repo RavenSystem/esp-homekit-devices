bbfadb8271d7c0105b247ca25117e5e999cbde98
RavenSystem <ravensystem@icloud.com>
2018-03-12 22:27:57 +0100

    Change pause duration to be fast

--- libs/led_codes/led_codes.c
+++ libs/led_codes/led_codes.c
@@ -23,10 +23,10 @@
 
 #include "led_codes.h"
 
-#define DURATION_OFF        200     / portTICK_PERIOD_MS
+#define DURATION_OFF        150     / portTICK_PERIOD_MS
 
 void led_code(uint8_t gpio, blinking_params params) {
-    uint32_t duration = ((params.duration * 1000) + 100) / portTICK_PERIOD_MS;
+    uint32_t duration = ((params.duration * 1000) + 90) / portTICK_PERIOD_MS;
     
     uint8_t i;
     for (i = 0; i < params.times; i++) {
