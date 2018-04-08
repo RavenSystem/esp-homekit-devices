760a3039e0e180e2024695a5c2cdf408b506b0eb
RavenSystem <ravensystem@icloud.com>
2018-04-08 15:47:14 +0200

    Rewrite to use ETSTimer instead vTaskDelay for better memory management

--- libs/led_codes/led_codes.c
+++ libs/led_codes/led_codes.c
@@ -19,22 +19,52 @@
 #include <stdio.h>
 #include <esp8266.h>
 #include <FreeRTOS.h>
-#include <task.h>
+#include <etstimer.h>
+#include <esplibs/libmain.h>
 
 #include "led_codes.h"
 
-#define DURATION_OFF        150     / portTICK_PERIOD_MS
+#define DURATION_OFF        150
 
-void led_code(uint8_t gpio, blinking_params params) {
-    uint32_t duration = ((params.duration * 1000) + 90) / portTICK_PERIOD_MS;
+typedef struct led_params_t {
+    blinking_params_t blinking_params;
+    uint8_t gpio;
+    bool status;
+    ETSTimer timer;
+    uint8_t count;
+    uint32_t delay;
+} led_params_t;
+
+led_params_t led_params;
+
+void led_code_run() {
+    led_params.status = !led_params.status;
     
-    uint8_t i;
-    for (i = 0; i < params.times; i++) {
-        if (i != 0) {
-            vTaskDelay(DURATION_OFF);
-        }
-        gpio_write(gpio, 0);
-        vTaskDelay(duration);
-        gpio_write(gpio, 1);
+    gpio_write(led_params.gpio, led_params.status);
+    
+    if (led_params.status == 0) {
+        led_params.delay = (led_params.blinking_params.duration * 1000) + 90;
+    } else {
+        led_params.delay = DURATION_OFF;
     }
+    
+    sdk_os_timer_disarm(&led_params.timer);
+    
+    if (led_params.count < led_params.blinking_params.times) {
+        led_params.count++;
+        sdk_os_timer_arm(&led_params.timer, led_params.delay, 0);
+    }
+}
+
+void led_code(uint8_t gpio, blinking_params_t blinking_params) {
+    led_params.gpio = gpio;
+    led_params.blinking_params = blinking_params;
+    
+    sdk_os_timer_disarm(&led_params.timer);
+    sdk_os_timer_setfn(&led_params.timer, led_code_run, NULL);
+    
+    led_params.status = 1;
+    led_params.count = 0;
+    
+    led_code_run();
 }
