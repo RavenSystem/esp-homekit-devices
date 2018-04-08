2611546a5c5f00bc0be3799ec4c34cc420000c9b
RavenSystem <ravensystem@icloud.com>
2018-04-08 15:46:08 +0200

    Some changes in type names

--- libs/led_codes/led_codes.h
+++ libs/led_codes/led_codes.h
@@ -18,21 +18,21 @@
 
 #pragma once
 
-typedef struct blinking_params {
+typedef struct blinking_params_t {
     uint8_t times;
     uint8_t duration;
-} blinking_params;
+} blinking_params_t;
 
-#define GENERIC_ERROR               (blinking_params){6,0}
-#define SENSOR_ERROR                (blinking_params){5,0}
-#define WIFI_CONNECTED              (blinking_params){1,2}
-#define IDENTIFY_ACCESSORY          (blinking_params){1,3}
-#define RESTART_DEVICE              (blinking_params){2,2}
-#define WIFI_CONFIG_RESET           (blinking_params){2,0}
-#define EXTRA_CONFIG_RESET          (blinking_params){2,1}
-#define FUNCTION_A                  (blinking_params){1,0}
-#define FUNCTION_B                  (blinking_params){2,0}
-#define FUNCTION_C                  (blinking_params){3,0}
-#define FUNCTION_D                  (blinking_params){4,0}
+#define GENERIC_ERROR               (blinking_params_t){6,0}
+#define SENSOR_ERROR                (blinking_params_t){5,0}
+#define WIFI_CONNECTED              (blinking_params_t){1,2}
+#define IDENTIFY_ACCESSORY          (blinking_params_t){1,3}
+#define RESTART_DEVICE              (blinking_params_t){2,2}
+#define WIFI_CONFIG_RESET           (blinking_params_t){2,0}
+#define EXTRA_CONFIG_RESET          (blinking_params_t){2,1}
+#define FUNCTION_A                  (blinking_params_t){1,0}
+#define FUNCTION_B                  (blinking_params_t){2,0}
+#define FUNCTION_C                  (blinking_params_t){3,0}
+#define FUNCTION_D                  (blinking_params_t){4,0}
 
-void led_code(uint8_t gpio, blinking_params params);
+void led_code(uint8_t gpio, blinking_params_t blinking_params);
