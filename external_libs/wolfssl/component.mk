05baa40535b449bc2deeb3c5427e8749ae2608fb
RavenSystem <ravensystem@icloud.com>
2018-04-17 11:24:07 +0200

    Remove unnecessary flags

--- external_libs/wolfssl/component.mk
+++ external_libs/wolfssl/component.mk
@@ -7,13 +7,7 @@ INC_DIRS += $(wolfssl_ROOT) $(wolfssl_THIRDPARTY_ROOT)
 wolfssl_INC_DIR = $(wolfssl_THIRDPARTY_ROOT)
 wolfssl_SRC_DIR = $(wolfssl_THIRDPARTY_ROOT)/src $(wolfssl_THIRDPARTY_ROOT)/wolfcrypt/src
 
-EXTRA_CFLAGS += -DWOLFSSL_USER_SETTINGS \
-    -DNO_SESSION_CACHE \
-    -DRSA_LOW_MEM \
-    -DGCM_SMALL \
-#    -DCURVE25519_SMALL \
-#    -DED25519_SMALL \
-    -DUSE_SLOW_SHA512
+EXTRA_CFLAGS += -DWOLFSSL_USER_SETTINGS
 
 $(eval $(call component_compile_rules,wolfssl))
 
