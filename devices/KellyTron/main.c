c2ce7843768c92343e545bcbd9adff3638395457
RavenSystem <ravensystem@icloud.com>
2018-12-25 13:12:28 +0100

    Version 0.0.1

--- devices/KellyTron/main.c
+++ devices/KellyTron/main.c
@@ -40,10 +40,13 @@
 
 #define TOGGLE_GPIO     5
 #define RELAY_GPIO      4
-#define DISABLED_TIME   50
+#define DEBOUNCE_TIME   40
+#define DISABLE_TIME    80
 
-static volatile uint32_t last_press_time;
-static ETSTimer device_restart_timer, change_settings_timer, extra_func_timer;
+bool gpio_state;
+uint8_t reset_toggle_counter = 0;
+volatile uint32_t disable_time;
+ETSTimer device_restart_timer, change_settings_timer, save_states_timer, toggle_switch_timer, factory_default_timer;
 
 void switch1_on_callback(homekit_value_t value);
 homekit_value_t read_switch1_on_callback();
@@ -55,24 +58,45 @@ void ota_firmware_callback();
 void on_wifi_ready();
 
 homekit_characteristic_t switch1_on = HOMEKIT_CHARACTERISTIC_(ON, false, .getter=read_switch1_on_callback, .setter=switch1_on_callback);
-
 homekit_characteristic_t show_setup = HOMEKIT_CHARACTERISTIC_(CUSTOM_SHOW_SETUP, false, .callback=HOMEKIT_CHARACTERISTIC_CALLBACK(show_setup_callback));
-
 homekit_characteristic_t reboot_device = HOMEKIT_CHARACTERISTIC_(CUSTOM_REBOOT_DEVICE, false, .id=103, .callback=HOMEKIT_CHARACTERISTIC_CALLBACK(ota_firmware_callback));
 homekit_characteristic_t ota_firmware = HOMEKIT_CHARACTERISTIC_(CUSTOM_OTA_UPDATE, false, .id=110, .callback=HOMEKIT_CHARACTERISTIC_CALLBACK(ota_firmware_callback));
+homekit_characteristic_t custom_init_state_sw1 = HOMEKIT_CHARACTERISTIC_(CUSTOM_INIT_STATE_SW1, 0, .id=120, .callback=HOMEKIT_CHARACTERISTIC_CALLBACK(change_settings_callback));
+
+// ---------------------------
 
 void relay_write(bool on, int gpio) {
-    last_press_time = xTaskGetTickCountFromISR() + (DISABLED_TIME / portTICK_PERIOD_MS);
+    disable_time = xTaskGetTickCountFromISR();
     gpio_write(gpio, on ? 1 : 0);
 }
 
+void identify_task() {
+    relay_write(true, RELAY_GPIO);
+    for (uint8_t i = 0; i < 3; i++) {
+        vTaskDelay(500 / portTICK_PERIOD_MS);
+        relay_write(true, RELAY_GPIO);
+        vTaskDelay(500 / portTICK_PERIOD_MS);
+        relay_write(false, RELAY_GPIO);
+    }
+    
+    vTaskDelete(NULL);
+}
+
 void save_settings() {
+    printf("KT >>> Saving settings\n");
+    
     sysparam_status_t status;
     bool bool_value;
+    int8_t int8_value;
     
     status = sysparam_get_bool("show_setup", &bool_value);
     if (status == SYSPARAM_OK && bool_value != show_setup.value.bool_value) {
-        sysparam_set_bool("show_setup", show_setup.value.bool_value);
+        status = sysparam_set_bool("show_setup", show_setup.value.bool_value);
+    }
+    
+    status = sysparam_get_int8("init_state_sw1", &int8_value);
+    if (status == SYSPARAM_OK && int8_value != custom_init_state_sw1.value.int_value) {
+        status = sysparam_set_int8("init_state_sw1", custom_init_state_sw1.value.int_value);
     }
 }
 
@@ -89,27 +113,24 @@ void device_restart_task() {
 }
 
 void device_restart() {
-    printf(">>> Restarting device\n");
+    printf("KT >>> Restarting device\n");
     xTaskCreate(device_restart_task, "device_restart_task", 256, NULL, 1, NULL);
 }
 
-void show_setup_callback() {
-    if (show_setup.value.bool_value) {
-        sdk_os_timer_setfn(&device_restart_timer, device_restart, NULL);
-        sdk_os_timer_arm(&device_restart_timer, 5000, 0);
-    } else {
-        sdk_os_timer_disarm(&device_restart_timer);
+void reset_call_task() {
+    printf("KT >>> Resetting device to factory defaults\n");
+    sysparam_status_t status;
+    
+    vTaskDelay(3100 / portTICK_PERIOD_MS);
+    
+    status = sysparam_set_bool("show_setup", false);
+    status = sysparam_set_int8("init_state_sw1", 0);
+    
+    if (status != SYSPARAM_OK) {
+        printf("KT !!! ERROR Flash problem\n");
     }
     
-    save_settings();
-}
-
-void reset_call_task() {
-    printf(">>> Resetting device to factory defaults\n");
-    
-    vTaskDelay(100 / portTICK_PERIOD_MS);
-    sysparam_set_bool("show_setup", false);
-    vTaskDelay(100 / portTICK_PERIOD_MS);
+    vTaskDelay(500 / portTICK_PERIOD_MS);
     
     homekit_server_reset();
     vTaskDelay(500 / portTICK_PERIOD_MS);
@@ -122,10 +143,41 @@ void reset_call_task() {
     vTaskDelete(NULL);
 }
 
-void reset_call(const uint8_t gpio) {
-    relay_write(false, RELAY1_GPIO);
-    gpio_disable(RELAY1_GPIO);
-    xTaskCreate(reset_call_task, "reset_call_task", 256, NULL, 1, NULL);
+void save_states() {
+    printf("KT >>> Saving last states\n");
+    
+    sysparam_status_t status;
+    bool bool_value;
+    
+    if (custom_init_state_sw1.value.int_value > 1) {
+        status = sysparam_get_bool("last_state_sw1", &bool_value);
+        if (status == SYSPARAM_OK && bool_value != switch1_on.value.bool_value) {
+            status = sysparam_set_bool("last_state_sw1", switch1_on.value.bool_value);
+        }
+    }
+}
+
+void factory_default() {
+    printf("KT >>> Checking factory default call\n");
+    
+    if (reset_toggle_counter > 12) {
+        gpio_disable(TOGGLE_GPIO);
+        xTaskCreate(identify_task, "identify_task", 256, NULL, 1, NULL);
+        xTaskCreate(reset_call_task, "reset_call_task", 256, NULL, 1, NULL);
+    }
+    
+    reset_toggle_counter = 0;
+}
+
+void show_setup_callback() {
+    if (show_setup.value.bool_value) {
+        sdk_os_timer_setfn(&device_restart_timer, device_restart, NULL);
+        sdk_os_timer_arm(&device_restart_timer, 5000, 0);
+    } else {
+        sdk_os_timer_disarm(&device_restart_timer);
+    }
+
+    save_settings();
 }
 
 void ota_firmware_callback() {
@@ -142,53 +194,128 @@ void change_settings_callback() {
     sdk_os_timer_arm(&change_settings_timer, 3000, 0);
 }
 
+void save_states_callback() {
+    sdk_os_timer_disarm(&save_states_timer);
+    sdk_os_timer_arm(&save_states_timer, 3000, 0);
+}
+
 void switch1_on_callback(homekit_value_t value) {
-    printf(">>> Toggle Switch 1\n");
+    printf("KT >>> Toggle Switch\n");
     switch1_on.value = value;
     relay_write(switch1_on.value.bool_value, RELAY_GPIO);
-    printf(">>> Relay 1 -> %i\n", switch1_on.value.bool_value);
+    printf("KT >>> Relay -> %i\n", switch1_on.value.bool_value);
+    save_states_callback();
 }
 
 homekit_value_t read_switch1_on_callback() {
     return switch1_on.value;
 }
 
-void toggle_switch(const uint8_t gpio) {
-    printf(">>> Toggle Switch manual\n");
-    switch1_on.value.bool_value = !switch1_on.value.bool_value;
-    relay_write(switch1_on.value.bool_value, RELAY_GPIO);
-    printf(">>> Relay 1 -> %i\n", switch1_on.value.bool_value);
-    homekit_characteristic_notify(&switch1_on, switch1_on.value);
+IRAM void toggle_switch_intr_callback(const uint8_t gpio) {
+    gpio_state = gpio_read(TOGGLE_GPIO);
+    sdk_os_timer_arm(&toggle_switch_timer, DEBOUNCE_TIME, 0);
+}
+
+IRAM void toggle_switch_callback() {
+    sdk_os_timer_disarm(&toggle_switch_timer);
+    
+    const uint32_t now = xTaskGetTickCountFromISR();
+    
+    if (now - disable_time > DISABLE_TIME / portTICK_PERIOD_MS) {
+        if (gpio_read(TOGGLE_GPIO) == gpio_state) {
+            printf("KT >>> Toggle Switch manual\n");
+            switch1_on.value.bool_value = !switch1_on.value.bool_value;
+            relay_write(switch1_on.value.bool_value, RELAY_GPIO);
+            printf("KT >>> Relay -> %i\n", switch1_on.value.bool_value);
+            homekit_characteristic_notify(&switch1_on, switch1_on.value);
+            save_states_callback();
+            
+            reset_toggle_counter++;
+            sdk_os_timer_arm(&factory_default_timer, 3200, 0);
+        }
+    }
 }
 
 void identify(homekit_value_t _value) {
-    printf(">>> Identify\n");
+    printf("KT >>> Identify\n");
+    xTaskCreate(identify_task, "identify_task", 256, NULL, 1, NULL);
 }
 
-void gpio_init() {
+void hardware_init() {
     sysparam_status_t status;
     bool bool_value;
     
     status = sysparam_get_bool("show_setup", &bool_value);
     if (status == SYSPARAM_OK) {
         show_setup.value.bool_value = bool_value;
-        printf(">>> Loading show_setup -> %i\n", show_setup.value.bool_value);
+        printf("KT >>> Loading show_setup -> %i\n", show_setup.value.bool_value);
     } else {
         sysparam_set_bool("show_setup", false);
-        printf(">>> Setting show_setup to default -> false\n");
+        printf("KT >>> Setting show_setup to default -> false\n");
     }
     
-    printf(">>> Loading device gpio settings\n");
-    
-    gpio_enable(TOGGLE_GPIO, GPIO_INPUT);
-    gpio_set_pullup(TOGGLE_GPIO->gpio, false, false);
+    printf("KT >>> Loading device gpio settings\n");
     
     gpio_enable(RELAY_GPIO, GPIO_OUTPUT);
     relay_write(switch1_on.value.bool_value, RELAY_GPIO);
     
+    gpio_enable(TOGGLE_GPIO, GPIO_INPUT);
+    gpio_set_pullup(TOGGLE_GPIO, false, false);
+    gpio_set_interrupt(TOGGLE_GPIO, GPIO_INTTYPE_EDGE_ANY, toggle_switch_intr_callback);
+    
+    sdk_os_timer_setfn(&toggle_switch_timer, toggle_switch_callback, NULL);
     sdk_os_timer_setfn(&change_settings_timer, save_settings, NULL);
+    sdk_os_timer_setfn(&save_states_timer, save_states, NULL);
+    sdk_os_timer_setfn(&factory_default_timer, factory_default, NULL);
     
     wifi_config_init("KellyTron", NULL, on_wifi_ready);
+}
+
+void settings_init() {
+    // Initial Setup
+    sysparam_status_t status;
+    bool bool_value;
+    int8_t int8_value;
+    
+    // Load Saved Settings and set factory values for missing settings
+    printf("KT >>> Loading settings\n");
+    
+    status = sysparam_get_bool("show_setup", &bool_value);
+    if (status == SYSPARAM_OK) {
+        show_setup.value.bool_value = bool_value;
+    } else {
+        status = sysparam_set_bool("show_setup", false);
+    }
+    status = sysparam_get_int8("init_state_sw1", &int8_value);
+    if (status == SYSPARAM_OK) {
+        custom_init_state_sw1.value.int_value = int8_value;
+    } else {
+        status = sysparam_set_int8("init_state_sw1", 0);
+    }
+    
+    // Load Saved States
+    printf("KT >>> Loading saved states\n");
+    
+    if (custom_init_state_sw1.value.int_value > 1) {
+        status = sysparam_get_bool("last_state_sw1", &bool_value);
+        if (status == SYSPARAM_OK) {
+            if (custom_init_state_sw1.value.int_value == 2) {
+                switch1_on.value.bool_value = bool_value;
+            } else {
+                switch1_on.value.bool_value = !bool_value;
+            }
+        } else {
+            status = sysparam_set_bool("last_state_sw1", false);
+        }
+    } else if (custom_init_state_sw1.value.int_value == 1) {
+        switch1_on.value.bool_value = true;
+    }
+    
+    if (status == SYSPARAM_OK) {
+        hardware_init();
+    } else {
+        printf("KT !!! ERROR in settings_init loading settings. Flash problem\n");
+    }
 }
 
 homekit_characteristic_t name = HOMEKIT_CHARACTERISTIC_(NAME, NULL);
@@ -197,10 +324,10 @@ homekit_characteristic_t serial = HOMEKIT_CHARACTERISTIC_(SERIAL_NUMBER, NULL);
 homekit_characteristic_t model = HOMEKIT_CHARACTERISTIC_(MODEL, "KellyTron");
 homekit_characteristic_t identify_function = HOMEKIT_CHARACTERISTIC_(IDENTIFY, identify);
 
-homekit_characteristic_t switch1_service_name = HOMEKIT_CHARACTERISTIC_(NAME, "Switch 1");
+homekit_characteristic_t switch1_service_name = HOMEKIT_CHARACTERISTIC_(NAME, "Switch");
 
 homekit_characteristic_t setup_service_name = HOMEKIT_CHARACTERISTIC_(NAME, "Setup", .id=100);
-homekit_characteristic_t device_type_name = HOMEKIT_CHARACTERISTIC_(CUSTOM_DEVICE_TYPE_NAME, "Switch 1", .id=101);
+homekit_characteristic_t device_type_name = HOMEKIT_CHARACTERISTIC_(CUSTOM_DEVICE_TYPE_NAME, "Switch", .id=101);
 
 homekit_characteristic_t firmware = HOMEKIT_CHARACTERISTIC_(FIRMWARE_REVISION, "0.0.1");
 
@@ -222,7 +349,7 @@ void create_accessory_name() {
 homekit_server_config_t config;
 
 void create_accessory() {
-    uint8_t service_count = 3, service_number = 2;
+    uint8_t service_count = 3;
     
     // Total service count
     if (show_setup.value.bool_value) {
@@ -248,12 +375,8 @@ void create_accessory() {
     sonoff_info->characteristics[4] = &firmware;
     sonoff_info->characteristics[5] = &identify_function;
     
-    printf(">>> Creating accessory\n");
-    
-    char *device_type_name_value = malloc(9);
-    snprintf(device_type_name_value, 9, "Switch 1");
-    device_type_name.value = HOMEKIT_STRING(device_type_name_value);
-    
+    printf("KT >>> Creating accessory\n");
+
     homekit_service_t *sonoff_switch1 = sonoff->services[1] = calloc(1, sizeof(homekit_service_t));
     sonoff_switch1->id = 8;
     sonoff_switch1->type = HOMEKIT_SERVICE_SWITCH;
@@ -265,12 +388,12 @@ void create_accessory() {
     
     // Setup Accessory, visible only in 3party Apps
     if (show_setup.value.bool_value) {
-        homekit_service_t *sonoff_setup = sonoff->services[service_number] = calloc(1, sizeof(homekit_service_t));
+        homekit_service_t *sonoff_setup = sonoff->services[2] = calloc(1, sizeof(homekit_service_t));
         sonoff_setup->id = 99;
         sonoff_setup->type = HOMEKIT_SERVICE_CUSTOM_SETUP;
         sonoff_setup->primary = false;
         
-        uint8_t setting_count = 4, setting_number = 3;
+        uint8_t setting_count = 5;
         
         sysparam_status_t status;
         char *char_value;
@@ -284,9 +407,9 @@ void create_accessory() {
         sonoff_setup->characteristics[0] = &setup_service_name;
         sonoff_setup->characteristics[1] = &device_type_name;
         sonoff_setup->characteristics[2] = &reboot_device;
+        sonoff_setup->characteristics[3] = &custom_init_state_sw1;
         if (status == SYSPARAM_OK) {
-            sonoff_setup->characteristics[setting_number] = &ota_firmware;
-            setting_number++;
+            sonoff_setup->characteristics[4] = &ota_firmware;
         }
     }
     
@@ -304,10 +427,9 @@ void on_wifi_ready() {
 void user_init(void) {
     uart_set_baud(0, 115200);
     
-    printf(">>> KellyTron firmware loaded\n");
-    printf(">>> Developed by RavenSystem - José Antonio Jiménez\n");
-    printf(">>> Firmware revision: %s\n\n", firmware.value.string_value);
+    printf("KT >>> KellyTron firmware loaded\n");
+    printf("KT >>> Developed by RavenSystem - José Antonio Jiménez\n");
+    printf("KT >>> Firmware revision: %s\n\n", firmware.value.string_value);
     
-    gpio_init();
+    settings_init();
 }
-
