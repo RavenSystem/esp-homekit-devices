111288d0854aeadfc715a0b384094d23d5360454
David Dix <david@dixieworld.co.uk>
2020-05-25 23:21:01 +0100

    Add local.mk with ESP settings

---
+++ devices/HAA/local.mk
@@ -0,0 +1,11 @@
+# Settings for the ESP Dev board
+FLASH_SIZE = 32
+FLASH_MODE = dio
+FLASH_SPEED = 80
+ESPPORT = "/dev/tty.SLAB_USBtoUART"
+
+# Settings for a Sonoff Basic
+# FLASH_SIZE = 8
+# FLASH_MODE = dout
+# FLASH_SPEED = 40
+# ESPPORT = "/dev/tty.usbserial-A50285BI"
