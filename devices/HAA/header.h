6c99dbc4482f7009476c464235bf378fbd847930
RavenSystem <ravensystem@icloud.com>
2020-05-30 13:07:33 +0200

    Home Accessory Architect v2.4.1

--- devices/HAA/header.h
+++ devices/HAA/header.h
@@ -9,8 +9,8 @@
 #define __HAA_HEADER_H__
 
 // Version
-#define FIRMWARE_VERSION                    "2.4.0"
-#define FIRMWARE_VERSION_OCTAL              020400      // Matches as example: firmware_revision 2.3.8 = 02.03.10 (octal) = config_number 020310
+#define FIRMWARE_VERSION                    "2.4.1"
+#define FIRMWARE_VERSION_OCTAL              020401      // Matches as example: firmware_revision 2.3.8 = 02.03.10 (octal) = config_number 020310
 
 // Sysparam
 #define SYSPARAMSECTOR                      0xF3000
