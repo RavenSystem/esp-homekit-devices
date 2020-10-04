d5098aee3a8e8ed3a95eaf6e4f1ac941505ce162
lougovsk <lougovsk@Jupiter.localdomain>
2020-10-04 15:20:16 +0200

    removed debugging printouts

--- devices/HAA/haa_lightbulb_driver.c
+++ devices/HAA/haa_lightbulb_driver.c
@@ -1,13 +1,10 @@
 #include "haa_lightbulb_driver.h"
 #include "stdlib.h"
-#include "header.h"
 
 void haa_lighbulb_driver_set_all(const driver_interface_t* interface, const uint16_t* multipwm_duty)
 {
-    INFO("WWWW 2.1\n");
     if((interface->setter != NULL) && (interface->updater != NULL))
     {
-        INFO("WWWW 2.2\n");
         for (uint8_t channel = 0; channel < interface->descriptor->numChannelsPerChip; channel++)
         {
             for (uint8_t chip = 0; chip < interface->descriptor->numChips; chip++)
